ee19aaf2a671348ff44db80695587b255f12e3b6
jraynovic <67034373+jraynovic@users.noreply.github.com>
2022-03-04 14:15:17 -0800

    fix: model build: structure for schema

--- models/Links.js
+++ models/Links.js
@@ -1,13 +1,11 @@
-export default (sequelize, DataTypes)=>{
-  const Links = sequelize.define(
-    'Links',{
-      id: {
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-        autoIncrement: true
-      },
-      url: {type: DataTypes.STRING},
-      slug:{tpye: DataTypes.STRING}
-    }
-  )
-}
+export default (sequelize, DataTypes) => {
+  const Links = sequelize.define("Links", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true
+    },
+    url: { type: DataTypes.STRING },
+    slug: { type: DataTypes.STRING, unique: true }
+  });
+};
